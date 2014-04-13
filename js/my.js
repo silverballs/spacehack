cebf47662799c9f179d3ead47a535098181ed62b
Silverballs <asilbersmith11@gmail.com>
2014-04-12 23:00:24 -0400

    Pulling in changes

--- js/my.js
+++ js/my.js
@@ -1,46 +1,74 @@
-//var game = function (time, package,  source, destination) {
-//logic
-//}
-
-var space = {
-    var clock,
-	origin = Number,
-	destination = Number,
-	newNode;
-
-    var package = {};
-    var node = {};
+    //assume there is json that exists, structured as data.nodes.property
     
-    //is dead or alive
+    //what they are passing
+    var package = {
+	isDelivered : function () {
+	    return this.isDelivered;
+	}
+	//not sure if this is needed 
+	//isOutForDelivery : function () {
+	//    return this.isOutForDelivery;
+	//}
+    };
 
-    origin.distance = 0;
-    destination.distance = undefined;
+    //the various satellites
+    var nodes = _.toArray(data.nodes);
 
-    //beginning
-    //origin = currentNode; 
+    //depending on how the json is structured, it might make sense to apply
+    //the satellite template to all nodes
 
-    var isNearMe = function (currentNode, newNode) {
-	var pi = 22 / 7; 
-	var distanceBetweenNodes = newNode.distance - currentNode.distance;
-	var passPacket = function (nextNode, presentNode) {
-	    presentNode.hasPackage = false;
-	    nextNode.hasPackage = true;
-	};
-	var vicinitiy = (2 * (pi * distanceBetweenNodes));
-	
-	if (currentNode.distance < vicinity) {
-	    if (newNode.isDestination) {
-		clock = 0;
+    //what is a satellite?
+    var satellite = {
+	location : function () {
+	    return this.location;
+	},
+	time : function () {
+	    return this.time;
+	},
+	currentLocation : function ( ) {
+	    return (this.location * this.time);
+	},
+	isDestination : undefined, 
+	hasPacket : undefined
+    };
+    
+    //lets make the start and end be satellite thingies 
+    var origin = {}, destination = {};
+    _.extend(origin, satellite);
+    _.extend(destination, satellite);
+    origin.hasPacket = true;
+    destination.isDestination = true;
+
+    var findNodesNearBy = function (focus, others) {
+	var neighbors = [];
+
+	_.each(others, function (other) {
+	    //some math that tells me your a neighbor
+	    if (mathStuff) {
+		neighbors.push(other);
+	    } else {
+		return neighbors;
 	    }
-	    passPacket(newNode, currentNode);
+	});
+
+	if (neighbors.length > 0) {
+	    var passPacket = function (neighbor) {
+		neighbor.hasPacket = true;
+	    };
+	    _.map(neighbors, passPacket(neighbor));
+	}
+	return neighbors;
+    };
+
+    var beginVisualization = function (origin , nodes) {
+	var nearByNodes = findNodesNearBy(origin, nodes);
+	
+	if (_.findWhere(nearByNodes, isDestination) == undefined) {
+	    _.map(nearByNodes, findNodesNearBy(node, nodes));
+	} else {
+	    alert('Packet has been delivered');
+	    //end vis
 	}
     }
 
-    if (package.isDelivered) {
-	clock === 0;
-    } else {
-	package.isOutForDelivery;
-	isNearMe(nodes);
-	
-    }
-}
+    beginVisualization(origin, nodes);
